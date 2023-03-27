db2dcb064bacf717946f314f39035960d9c4931f
Ghostinho <83088719+Ghostinho@users.noreply.github.com>
2023-03-26 21:05:19 -0300

    Create Python_08.py

---
+++ Python_08.py
@@ -0,0 +1,7 @@
+mt = float(input('Digte um valor em metros: '))
+cm = mt * 100
+mm = mt * 1000
+dm = mt / 10
+hm = mt / 100
+print(f'{mt}m equivale há \n{cm}cm \n{mm}mm \n{mt/1000}km')
+print(f'{hm}hm \n{dm}dm')
