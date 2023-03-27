2c9f0bc5d0185b0e2b2d02e1c0eb7e609ea46f00
Ghostinho <83088719+Ghostinho@users.noreply.github.com>
2023-03-26 21:09:16 -0300

    Create Python_11.py

---
+++ Python_11.py
@@ -0,0 +1,4 @@
+lg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+print(f'Sua parede tem uma dimensao de {lg}x{alt} e a usa aréa de {lg*alt:.3f}m²')
+print(f'Para pintar essa parede voçê precisa de {(lg*alt)/2}l de tinta')
