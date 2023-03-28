85aec3a2f52dbf528b42fd127f1a2ba0e835102b
Ghostinho <83088719+Ghostinho@users.noreply.github.com>
2023-03-27 23:24:59 -0300

    Update Python_12.py

--- Python_12.py
+++ Python_12.py
@@ -1,4 +1,4 @@
 preço = float(input('Qual o preço do produto? R$ '))
-d = float(input('Digite o valor do desconto: '))
-total = preço - (preço * d / 100)
-print(f'O produto que custa, {preço} no desconto de {d:.0f} custa: R${total:.2f}')
+produto = float(input('Digite o valor do desconto: '))
+total = preço - (preço * produto / 100)
+print(f'O produto que custa, {preço} no desconto de {produto:.0f} custa: R${total:.2f}')
